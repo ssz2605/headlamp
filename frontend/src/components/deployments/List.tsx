383386752e3cf9344809e37f21749dac4313cc87
Shreya Saxena <shreyasaxena2605@gmail.com>
2026-07-16 23:58:50 +0530

    frontend: Guard against missing Deployment status in list

--- frontend/src/components/deployments/List.tsx
+++ frontend/src/components/deployments/List.tsx
@@ -94,7 +94,7 @@ export default function DeploymentsList() {
           id: 'pods',
           label: t('Pods'),
           disableFiltering: true,
-          getValue: deployment => deployment.status.availableReplicas,
+          getValue: deployment => deployment.status?.availableReplicas,
           render: deployment => renderPods(deployment),
           sort: sortByPods,
           gridTemplate: 'min-content',
